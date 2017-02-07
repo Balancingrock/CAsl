009477f066096c325c031aed4ed2b01b7644abcb
Rien <github@balancingrock.nl>
2017-02-07 11:46:11 +0100

    Testing header fix

--- Sources/include/CAsl.h
+++ Sources/include/CAsl.h
@@ -7,10 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <asl.h>
 
 #ifndef CAsl_h
 #define CAsl_h
 
 int asl_bridge_log_message(int level, NSString *message);
+int asl_add_log_file(asl_object_t client, int descriptor);
 
 #endif /* CAsl_h */
